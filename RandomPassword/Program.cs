525ab3177128fe81d8dddc28a00a88df887bee56
Ahmet Maden <a.maden541@gmail.com>
2023-09-13 08:47:56 +0300

    random password

---
+++ RandomPassword/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace RndPassword {
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Şifre uzunluğunu giriniz :");
+            int passwordLenght = int.Parse(Console.ReadLine());
+
+            string password = SifreOlustur(passwordLenght);
+            Console.WriteLine("Oluşturulan şifre :" + password);
+            Console.ReadLine();
+        
+        
+        }
+    
+        static string SifreOlustur(int lenght)
+        {
+            while(true) { 
+             const string chars =
+               "ABCDEFGIHJKLMNOPQRSTUVWYZabcdefgıhjklmnopqrstuvwyz0123456789!@#$½&^?*()_+";
+            
+            StringBuilder sb = new StringBuilder();
+            Random random= new Random();
+            for (int i = 0; i < lenght; i++)
+            {
+                int index = random.Next(chars.Length);
+                sb.Append(chars[index]);
+            }
+            return sb.ToString();
+        
+            
+           
+            
+            
+            } 
+
+          
+           
+
+        }
+           
+}   
+    }
